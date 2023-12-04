98c48a0af400c3983ee003053802294651ad1aee
Nightkun <nthe7610@gmail.com>
2023-12-04 11:48:12 +0700

    Tinny commit

--- routers/mainrouter.js
+++ routers/mainrouter.js
@@ -5,12 +5,29 @@ router.get('/', (req,res)=>{
     res.render('index.ejs')
 })
 
+router.get('/payments', (req,res)=>{
+    res.render('payment')
+})
+
 router.get('/product', (req,res) =>{
     res.render('product')
+    res.status(404).sendFile(__dirname + 'images/404.png');
 })
 
 router.get('/contack', (req,res) =>{
     res.render('contack')
+})
+
+router.get('/auth/singup', (req,res)=> {
+    res.render('singup')
+})
+
+router.get('/auth/singin', (req,res)=> {
+    res.render('singin')
+})
+
+router.get('/profile', (req,res)=> {
+    res.render('profile')
 })
 
 module.exports = router
